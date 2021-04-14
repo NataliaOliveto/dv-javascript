80d58cd6a17e16db23a2e10061c52e61fb555886
NataliaOliveto <nataliaoliveto94@gmail.com>
2021-04-13 22:50:12 -0300

    Pequeña feature al Piedra, Papel o Tijeras

---
+++ 3-rock-paper-scissors/README.md
@@ -0,0 +1,28 @@
+# ROCK PAPER SCISSORS GAME
+
+## ¿CÓMO SE JUEGA?
+
+- Cada partida es al mejor de 3
+- Se debe seleccionar rock, paper or scissors de las tres opciones de la parte inferior. Al seleccionar, la computadora hará su elección inmediatamente
+- Se mostrará el resultado de esa mano en el recuadro celeste, y se sumará al resultado total en los contadores de la pantalla
+- Si el jugador alcanza 3 victorias, ganará la partida. Si el jugador es derrotado 3 veces, la perderá
+- Los contadores se reiniciarán cuando el jugador gane o pierda la partida
+
+---
+
+### DESARROLLO PASO A PASO
+
+1. Forkear el código del Piedra, Papel o Tijeras del [repositorio](https://github.com/giancarlol/dv-javascript)
+2. Redefinir el diseño
+3. Conseguir las imágenes necesarias
+4. Realizar el código:
+- Mejorar el <p id="result /> para que muestre la oración `Ganaste/Perdiste/Empataste con ${playerChoice} contra ${computerChoice}`
+- Agregar un <p id="counter" /> que muestre cuántas veces ganó el jugador y cuántas la computadora
+- EXTRA (Opcional) Que el <p id="counter" /> arranque en 0 - 0 y vaya sumando 1 cada vez que gana el jugador o la computadora
+- EXTRA (Opcional) El primero que llega a 3 victorias es el ganador real
+5. Testear y debuggear
+6. Crear el README.md
+7. Pushear cambios al repositorio forkeado y compartirlo
+
+---
+Créditos por las imágenes a attilacsanyi
--- 3-rock-paper-scissors/app.js
+++ 3-rock-paper-scissors/app.js
@@ -1,13 +1,19 @@
 const playerChoiceElement = document.getElementById("playerChoice");
 const computerChoiceElement = document.getElementById("computerChoice");
-
 const resultElement = document.getElementById("result");
-
 const buttons = document.querySelectorAll("button");
+const detailElement = document.getElementById("detail");
+const counterElement = document.getElementById("counter");
+const counterFinalElement = document.getElementById("counterFinal");
+const mensajeFinalElement = document.getElementById("mensajeFinal");
+
+var countWin = 0;
+var countLoss = 0;
 
 buttons.forEach((button) => button.addEventListener("click", startGame));
 
 function startGame(event) {
+
   // Obtener elección del jugador
   const button = event.currentTarget;
   const playerChoice = button.dataset.choice;
@@ -23,18 +29,17 @@ function startGame(event) {
   playerChoiceElement.setAttribute("src", `imgs/${playerChoice}.png`);
   computerChoiceElement.setAttribute("src", `imgs/${computerChoice}.png`);
 
-  // const result = playerWins ? "GANASTE" : "PERDISTE";
-
-  //   if (playerWins === true) {
-  //     result.textContent = "GANASTE";
-  //   } else if (!playerWins) {
-  //     result.textContent = "PERDISTE";
-  //   } else if (playerWins === "draw") {
-  //     result.textContent = "EMPATASTE";
-  //   }
-
+  // Settear ganador, elecciones y cantidad de victorias
   resultElement.textContent = winner;
-}
+  detailElement.textContent = `Con ${translateChoice(playerChoice)} contra ${translateChoice(computerChoice)}`;
+  counterElement.textContent = `Ganaste ${countWin} veces. Perdiste ${countLoss} veces`;
+  counterFinalElement.textContent = `${countWin} - ${countLoss}`;
+  mensajeFinalElement.textContent = ``;
+
+  // Validar si se continua el juego o se reinicia
+  continueGame();
+
+  }
 
 const possibleChoices = ["rock", "paper", "scissors"];
 
@@ -48,17 +53,47 @@ function getComputerChoice() {
 
 // Antes: isPlayerWinner
 function setWinner(playerChoice, computerChoice) {
-  console.log("playerChoice", playerChoice);
-  console.log("computerChoice", computerChoice);
+
   if (
     (playerChoice === "rock" && computerChoice === "scissors") ||
     (playerChoice === "paper" && computerChoice === "rock") ||
     (playerChoice === "scissors" && computerChoice === "paper")
   ) {
+    countWin++;
     return "GANASTE";
   } else if (playerChoice === computerChoice) {
     return "EMPATASTE";
   } else {
+    countLoss++;
     return "PERDISTE";
+  }
+}
+
+// Traducción de opciones al español
+function translateChoice(choice){
+  if(choice === "rock"){
+    return "roca";
+  } else if (choice === "paper"){
+    return "papel";
+  } else if (choice === "scissors"){
+    return "tijeras";
+  }
+}
+
+// Reiniciar el juego
+function endGame(){
+  countWin = 0;
+  countLoss = 0;
+  startGame;
+}
+
+// Validar si se continua o se reinicia
+function continueGame(){
+  if (countWin === 3){
+    mensajeFinalElement.textContent = `¡GANASTE, FELICITACIONES!`;
+    endGame();
+  } else if (countLoss === 3) {
+    mensajeFinalElement.textContent = `¡UNA LÁSTIMA, PERDISTE!`;
+    endGame();
   }
 }
